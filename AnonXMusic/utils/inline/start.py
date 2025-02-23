9f2085f4505f04f23f0f4756d0758fd1da627b45
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2025-02-23 12:07:34 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,10 +26,13 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=7765692814),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL), 
+            InlineKeyboardButton(text=_["S_B_0"], url=f"https://t.me/Cloning_music_Bot"), 
+         
         ],
     ]
     return buttons
